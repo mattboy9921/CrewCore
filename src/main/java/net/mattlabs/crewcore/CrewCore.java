d4ccf89e34b4f1676d6b4256baa991501609b372
mattboy9921 <mattboy9921@aim.com>
2019-07-12 22:33:25 -0400

    Add configuration for /ender command

--- src/main/java/net/mattlabs/crewcore/CrewCore.java
+++ src/main/java/net/mattlabs/crewcore/CrewCore.java
@@ -4,12 +4,14 @@ import co.aikar.commands.PaperCommandManager;
 import net.mattlabs.crewcore.commands.EnderCommand;
 import net.mattlabs.crewcore.listeners.JoinListener;
 import net.mattlabs.crewcore.listeners.QuitListener;
+import net.mattlabs.crewcore.util.ConfigManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CrewCore extends JavaPlugin {
 
     private static CrewCore instance;
     public PaperCommandManager paperCommandManager;
+    private ConfigManager configManager;
 
     public void onEnable() {
 
@@ -18,12 +20,21 @@ public class CrewCore extends JavaPlugin {
         // Register ACF
         paperCommandManager = new PaperCommandManager(this);
 
+        // Configuration Section
+        configManager = new ConfigManager(this);
+        configManager.loadConfigFiles(
+                new ConfigManager.ConfigPath(
+                        "config.yml",
+                        "config.yml",
+                        "config.yml"));
+        configManager.saveAllConfigs(false);
+
         // Register Listeners
         getServer().getPluginManager().registerEvents(new JoinListener(), this);
         getServer().getPluginManager().registerEvents(new QuitListener(), this);
 
         // Register Commands with ACF
-        paperCommandManager.registerCommand(new EnderCommand());
+        if (enderEnabled()) paperCommandManager.registerCommand(new EnderCommand());
     }
 
     public void onDisable() {
@@ -32,5 +43,9 @@ public class CrewCore extends JavaPlugin {
 
     public static CrewCore getInstance() {
         return instance;
+    }
+
+    private boolean enderEnabled() {
+        return configManager.getFileConfig("config.yml").getBoolean("ender");
     }
 }
