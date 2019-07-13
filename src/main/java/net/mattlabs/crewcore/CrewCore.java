21e1ba4f6a82e0fdff218b2f81e3ca38458e90fd
mattboy9921 <mattboy9921@aim.com>
2019-07-12 22:17:59 -0400

    Remove DiscordSRV

--- src/main/java/net/mattlabs/crewcore/CrewCore.java
+++ src/main/java/net/mattlabs/crewcore/CrewCore.java
@@ -1,33 +1,19 @@
 package net.mattlabs.crewcore;
 
 import net.mattlabs.crewcore.commands.EnderCommand;
-import net.mattlabs.crewcore.listeners.DiscordSRVListener;
 import net.mattlabs.crewcore.listeners.JoinListener;
 import net.mattlabs.crewcore.listeners.QuitListener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CrewCore extends JavaPlugin {
 
     private EnderCommand enderCommand;
 
-    private DiscordSRVListener discordSRVListener;
-    private boolean discordSRVEnabled;
     private static CrewCore instance;
 
     public void onEnable() {
 
         instance = this;
-
-        // DiscordSRV Check
-        if (!hasDiscordSRV()) {
-            this.getLogger().info(String.format("DiscordSRV not detected, disabling integration."));
-            discordSRVEnabled = false;
-        }
-        else {
-            this.getLogger().info(String.format("DiscordSRV detected, enabling integration."));
-            discordSRVEnabled = true;
-        }
 
         // Register Listeners
         getServer().getPluginManager().registerEvents(new JoinListener(), this);
@@ -44,14 +30,5 @@ public class CrewCore extends JavaPlugin {
 
     public static CrewCore getInstance() {
         return instance;
-    }
-
-    // DiscordSRV Helper Method
-    private boolean hasDiscordSRV() {
-        return getServer().getPluginManager().getPlugin("DiscordSRV") != null;
-    }
-
-    public boolean getDiscordSRVEnabled() {
-        return discordSRVEnabled;
     }
 }
